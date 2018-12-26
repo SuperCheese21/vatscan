b0534a1ebe530d2666ec6f1a7cb94787c12cee94
SuperCheese21 <ethan.shields21@gmail.com>
2018-12-26 13:48:42 +0800

    Removed sliding up panel and prep to add custom animation

--- src/components/InfoPanel.js
+++ src/components/InfoPanel.js
@@ -1,80 +1,56 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import SlidingUpPanel from 'rn-sliding-up-panel';
+import React from 'react';
+import { Animated, View } from 'react-native';
 
 import BasicData from './BasicData';
 import DetailData from './DetailData';
 
 import constants from '../config/constants.json';
 
-export default class InfoPanel extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            panelPosition: constants.panelStates.COLLAPSED
-        };
-        this.infoPanel = React.createRef();
+export default class InfoPanel extends React.Component {
+    state = {
+        panelPosition: constants.panelStates.COLLAPSED
     }
 
     getPanelPosition = () => {
         return this.state.panelPosition;
     }
 
-    setPanelPosition = position => {
-        console.log('Setting panel position to ' + position);
-        this.setState({
-            panelPosition: position
-        });
-        this.infoPanel.current.transitionTo({
-            toValue: position,
-            duration: 250
-        });
-    }
+    // setPanelPosition = position => {
+    //     console.log('Setting panel position to ' + position);
+    //     this.setState({
+    //         panelPosition: position
+    //     });
+    //     this.infoPanel.current.transitionTo({
+    //         toValue: position,
+    //         duration: 250
+    //     });
+    // }
 
-    adjustPanelPosition = position => {
-        const prevState = this.state.panelPosition;
-        const { COLLAPSED, EXPANDED, HALF_EXPANDED } = constants.panelStates;
-
-        if (prevState === EXPANDED) {
-            if (position > HALF_EXPANDED) {
-                this.setPanelPosition(HALF_EXPANDED);
-            } else {
-                this.setPanelPosition(COLLAPSED);
-            }
-        } else if (prevState === HALF_EXPANDED) {
-            if (position > HALF_EXPANDED) {
-                this.setPanelPosition(EXPANDED);
-            } else {
-                this.setPanelPosition(COLLAPSED);
-            }
-        }
-    }
+    // adjustPanelPosition = position => {
+    //     const prevState = this.state.panelPosition;
+    //     const { COLLAPSED, EXPANDED, HALF_EXPANDED } = constants.panelStates;
+    //
+    //     if (prevState === EXPANDED) {
+    //         if (position > HALF_EXPANDED) {
+    //             this.setPanelPosition(HALF_EXPANDED);
+    //         } else {
+    //             this.setPanelPosition(COLLAPSED);
+    //         }
+    //     } else if (prevState === HALF_EXPANDED) {
+    //         if (position > HALF_EXPANDED) {
+    //             this.setPanelPosition(EXPANDED);
+    //         } else {
+    //             this.setPanelPosition(COLLAPSED);
+    //         }
+    //     }
+    // }
 
     render() {
-        const top = constants.panelStates.EXPANDED;
-        const bottom = constants.panelStates.COLLAPSED;
         return (
-            <SlidingUpPanel
-                ref={this.infoPanel}
-                visible={true}
-                showBackdrop={false}
-                startCollapsed={true}
-                allowMomentum={false}
-                draggableRange={{
-                    top: top,
-                    bottom: bottom
-                }}
-                onDragEnd={position => this.adjustPanelPosition(position)}
-                onRequestClose={() => this.props.removeFocusedClient()}
-                height={top - bottom}
-            >
-
-                <View style={{ flex: 1 }}>
-                    <BasicData data={this.props.basicData} />
-                    <DetailData data={this.props.detailData} />
-                </View>
-
-            </SlidingUpPanel>
+            <View style={{ flex: 1 }}>
+                <BasicData data={this.props.basicData} />
+                <DetailData data={this.props.detailData} />
+            </View>
         );
     }
 }
