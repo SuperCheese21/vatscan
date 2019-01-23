04231350e7539a1853eb7828ef0001d7af6bb01f
SuperCheese21 <ethan.shields21@gmail.com>
2019-01-22 23:42:43 -0800

    Update app version, prepare to add stack navigator component

---
+++ src/components/screens/ClientScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import airportNames from '../../data/airportNames.json';
+
+const ClientScreen = props => (
+    <View style={{ flex: 1 }}>
+        <Text>Test</Text>
+    </View>
+);
+
+export default ClientScreen;
