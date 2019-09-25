0dc2cbc0e5c224dfdc59de7e808c09a7e9880bff
SuperCheese21 <ethan.shields21@gmail.com>
2019-09-24 19:17:42 -0700

    Updated dependencies, temporarily removed eslint universe config

--- App.js
+++ App.js
@@ -97,8 +97,7 @@ export default class App extends PureComponent {
     let focusedClient = {};
 
     // Find focused client inside updated client data array
-    for (const client of clients) {
-      // eslint-disable-line
+    for (const client of clients) { // eslint-disable-line
       if (focusedCallsign === client.callsign) {
         focusedClient = client;
         break;
